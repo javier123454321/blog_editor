be19067e709a5b26f2209b3fe1313547b31e3371
javier123454321 <javier.e.glz@gmail.com>
2026-01-25 03:54:35 +0000

    feat: US-007 - Initialize Vue 3 + TypeScript frontend project

---
+++ editor/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="app">
+    <h1>Blog Editor</h1>
+    <p>Welcome to the Blog Editor. Start editing your blog posts.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// App component
+</script>
+
+<style scoped>
+.app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #333;
+  padding: 2rem;
+}
+
+h1 {
+  margin: 0 0 1rem 0;
+  font-size: 2.5rem;
+}
+
+p {
+  font-size: 1.1rem;
+  color: #666;
+}
+</style>
